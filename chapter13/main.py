cb526fa95ea2614f03d4ad4871da25d56d8d6ce5
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2025-07-29 08:30:47 +0000

    Added API health check logic

--- chapter13/main.py
+++ chapter13/main.py
@@ -32,3 +32,21 @@ input_name_50 = sess_50.get_inputs()[0].name
 label_name_50 = sess_50.get_outputs()[0].name
 input_name_90 = sess_90.get_inputs()[0].name
 label_name_90 = sess_90.get_outputs()[0].name
+
+# FastAPI constructor with additional details added for OpenAPI Specification
+app = FastAPI(
+    description=api_description,
+    title="Fantasy acquisition API",
+    version="0.1",
+)
+
+@app.get(
+    "/",
+    summary="Check to see if the Fantasy acquisition API is running",
+    description="""Use this endpoint to check if the API is running. You can also check it first before making other calls to be sure it's running.""",
+    response_description="A JSON record with a message in it. If the API is running the message will say successful.",
+    operation_id="v0_health_check",
+    tags=["analytics"],
+)
+def root():
+    return {"message": "API health check successful"}
